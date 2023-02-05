dda5c36342fdc708e7d038c08b486e2f69fe7025
keta1 <k@ketal.icu>
2023-02-05 12:48:48 +0800

    Update kotlin, ktor

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,55 +1,50 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-
+@Suppress("DSL_SCOPE_VIOLATION")
 plugins {
-    kotlin("jvm") version "1.7.20"
-    kotlin("plugin.serialization") version "1.7.20"
-    id("com.github.johnrengelman.shadow") version "7.1.2"
+    alias(libs.plugins.kotlin)
+    alias(libs.plugins.serialization)
+    alias(libs.plugins.shadow)
     application
 }
 
 group = "icu.ketal"
 version = "1.0-SNAPSHOT"
 
-repositories {
-    mavenCentral()
-    maven("https://jitpack.io")
+application {
+    mainClass.set("MainKt")
 }
 
-val telegramBotApi = "3.2.6"
-val ktorVersion = "2.1.1"
-val exposedVersion = "0.39.2"
-
 dependencies {
+    implementation(libs.ktor.serialization.json)
+    implementation(libs.kotlin.logging)
+    implementation(libs.tgbotapi)
+    implementation(libs.ktor.server)
+    implementation(libs.exposed.core)
+    implementation(libs.exposed.dao)
+    runtimeOnly(libs.exposed.jdbc)
+    implementation(libs.exposed.kotlin.datetime)
+    implementation(libs.ktor.server.core)
+    implementation(libs.ktor.server.netty)
+    implementation(libs.ktor.client.core)
+    implementation(libs.ktor.client.okhttp)
+    implementation(libs.kotlinx.gettext)
+    runtimeOnly(libs.sqlite.jdbc)
+    runtimeOnly(libs.log4j.slf4j.impl)
+    runtimeOnly(libs.log4j.core)
+    runtimeOnly(libs.log4j.api)
     testImplementation(kotlin("test"))
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.4.0")
-    implementation("com.squareup.okhttp3:okhttp-tls:4.10.0")
-    implementation("io.github.microutils:kotlin-logging-jvm:2.1.23")
-    implementation("dev.inmo:tgbotapi:$telegramBotApi")
-    implementation("dev.inmo:micro_utils.ktor.server:0.12.13")
-    implementation("org.jetbrains.exposed:exposed-core:$exposedVersion")
-    implementation("org.jetbrains.exposed:exposed-dao:$exposedVersion")
-    runtimeOnly("org.jetbrains.exposed:exposed-jdbc:$exposedVersion")
-    implementation("org.jetbrains.exposed:exposed-kotlin-datetime:$exposedVersion")
-    implementation("io.ktor:ktor-server-core-jvm:$ktorVersion")
-    implementation("io.ktor:ktor-server-netty-jvm:$ktorVersion")
-    implementation("io.ktor:ktor-client-core-jvm:$ktorVersion")
-    implementation("io.ktor:ktor-client-okhttp:$ktorVersion")
-    implementation("name.kropp.kotlinx-gettext:kotlinx-gettext:0.5.0")
-    runtimeOnly("org.xerial:sqlite-jdbc:3.39.3.0")
-    runtimeOnly("org.apache.logging.log4j:log4j-slf4j-impl:2.19.0")
-    runtimeOnly("org.apache.logging.log4j:log4j-core:2.19.0")
-    runtimeOnly("org.apache.logging.log4j:log4j-api:2.19.0")
 }
 
 tasks.test {
     useJUnitPlatform()
 }
 
-tasks.withType<KotlinCompile> {
-    kotlinOptions.jvmTarget = "11"
-    kotlinOptions.freeCompilerArgs = listOf("-Xcontext-receivers")
-}
-
-application {
-    mainClass.set("MainKt")
+kotlin {
+    jvmToolchain(11)
+    target {
+        compilations.all {
+            kotlinOptions {
+                freeCompilerArgs += "-Xcontext-receivers"
+            }
+        }
+    }
 }
